bf03a17a7a8a60de6f771f6e3be849294ebca7e3
alessandragaleno <alessandragaleno421@gmail.com>
2024-03-10 14:47:33 -0300

    ultima atualização

--- cpf.dart
+++ cpf.dart
@@ -3,7 +3,8 @@ void main () {
     'Alexya' : 23468634345,
     'Alessandra' : 54790243999,
     'Paulo' : 36876498722,
-    'Gabriel' : 56723409812
+    'Gabriel' : 56723409812,
+    'José' : 98765432109
   };
   //O FOREACH PROCURA  UM VALOR EM UMA LISTA
   nomePessoa.forEach((key, value) {
